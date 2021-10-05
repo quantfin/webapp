b300efd67c3201c90cfb494001b2f4d50af49441
Akash Ranjan <akash.hallo@gmail.com>
2021-10-05 18:03:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var onDefault=function(req, res){
 app.get("/",onDefault);  // Request handler functions are registered
 app.get("/aboutus",onAboutUs);  // Request handler functions are registered
 var server=app.listen(8081);
-console.log("Server is running on port 8081");
+console.log("Server is running on port 9000");
 // if external packages are required
 // we have to download those packages from internet using  package manager tool
 // Runtime--------tools-------------packages-----------------------config file
